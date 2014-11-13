bba7ff0360bb397327f9cc91d20341ba32c522ba
smidra <ivanaveliskova@gmail.com>
2014-11-12 22:07:39 -0500

    updated airport

--- javascript.js
+++ javascript.js
@@ -1,29 +1,22 @@
-
-//Main function
-var main = function(){
-	//Starts working when the user hits the sumbit button.
-	$("#calcDistance").submit(function(){
-		//Sets the values from the selections on the form into variables
-		var depart = $("select#leaving").val();
-		var arrive = $("select#going").val();
-
-		//Uses the function within the flights.js to find the distances and sets it to a variable
-		var distance = IntentMedia.Distances.distance_between_airports(depart, arrive);
-
-		//if statement to determine whether the inputs of the user were valid.
-		//If not then the document is modified to tell the user to try again.
-		if(distance < 0){
-			$("#distances").text("This airport does not exist.");
-		}else if(distance === 0){
-			$("#distances").text("You are where you started!")
-		}else{
-			$("#distances").text(distance + " miles away")
-		}
-
-		//Prevents the document from submitting to a server to keep the page from refreshing
-		return false;
-	});
-}
-
-//Initiates the function only if the rest of the document has loaded.
-$(document).ready(main);
+var main = function () {
+     
+    document.getElementById('calcDistance').onsubmit=( function (e) {
+        // don't submit form on click
+        e.preventDefault();
+ 
+        var depart = document.getElementById('leaving').value,
+            arrive = document.getElementById('going').value,
+            distances = document.getElementById('distances'),
+            distance = IntentMedia.Distances.distance_between_airports(depart, arrive);
+ 
+        //if statement to determine whether the inputs of the user were valid.
+        //If not then the document is modified to tell the user to try again.
+        if (distance < 0) {
+            distances.innerHTML = "This airport does not exist.";
+        } else if (distance === 0){
+            distances.innerHTML = "You are where you started!";
+        } else {
+            distances.innerHTML = (distance + " miles away");
+        }
+    });
+}();
